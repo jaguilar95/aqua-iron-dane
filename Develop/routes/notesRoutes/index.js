0b6d196cef7d70c97072c57790026f8b3169146f
Leonardo <josue.aguilar1995@gmail.com>
2022-06-12 22:31:04 -0500

    finished delete call

--- Develop/routes/notesRoutes/index.js
+++ Develop/routes/notesRoutes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const notesRoutes = require("./notesRoutes");
 
-router.use(notesRoutes);
+router.use("/api", notesRoutes);
 
 module.exports = router;
--- Develop/routes/notesRoutes/notesRoutes.js
+++ Develop/routes/notesRoutes/notesRoutes.js
@@ -3,15 +3,17 @@ const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
 
-router.get("/api/notes", (req, res) => {
+router.get("/notes", (req, res) => {
   res.json(notes);
 });
 
-router.post("/api/notes", (req, res) => {
+router.post("/notes", (req, res) => {
+  // add id based on array length
   req.body.id = notes.length.toString();
 
   const noteData = req.body;
 
+  // validate response
   if (!noteData.title || typeof noteData.title !== "string") {
     res.status(400).send("The note is not properly formatted.");
   }
@@ -20,14 +22,36 @@ router.post("/api/notes", (req, res) => {
     res.status(400).send("The note is not properly formatted.");
   }
 
+  // add current note to notes array
   notes.push(noteData);
 
+  // save updated notes array to db.json
   fs.writeFileSync(
     path.join(__dirname, "../../db/db.json"),
     JSON.stringify(notes, null, 2)
   );
 
-  res.json(noteData);
+  // send current note as response
+  res.json({ message: "note successfully added", note: noteData });
+});
+
+router.delete("/notes/:id", (req, res) => {
+  // validate delete request
+  const currentNote = notes.filter((note) => note.id === req.params.id)[0];
+
+  if (!currentNote) {
+    res.status(404).send({ error: "Note not found" });
+  }
+
+  // create new array with selected note deleted
+  const filteredNotes = notes.filter((note) => note.id !== req.params.id);
+
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(filteredNotes, null, 2)
+  );
+
+  res.json({ message: "note deleted", note: currentNote });
 });
 
 module.exports = router;
